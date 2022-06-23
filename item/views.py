a805ec28f23e10a828f35ddc2563781e8619b3ac
mankic <miic0803@gmail.com>
2022-06-23 09:50:01 +0900

    수정 - serializer base

--- item/views.py
+++ item/views.py
@@ -2,5 +2,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Category
+from .models import Item
+from .models import Order
+from .models import ItemOrder
 # Create your views here.
+
+
 
