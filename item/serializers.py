7989171d7b93e5b25ec11eeeb18c4c5dfcfedee1
mankic <miic0803@gmail.com>
2022-06-24 00:12:59 +0900

    수정 - 7. 까지 구현

--- item/serializers.py
+++ item/serializers.py
@@ -1,35 +1,55 @@
 from rest_framework import serializers
 
 # from user.models import User as UserModel
-from item.models import Category as  CategoryModel
-from item.models import Item as  ItemModel
-from item.models import Order as  OrderModel
-from item.models import ItemOrder as  ItemOrderModel
+from item.models import Category as CategoryModel
+from item.models import Item as ItemModel
+from item.models import Order as OrderModel
+from item.models import ItemOrder as ItemOrderModel
 
 
 class CategorySerializer(serializers.ModelSerializer):
-               
+                
     class Meta:
         model = CategoryModel
-        fields = ['name']
+        fields = ["name"]
 
 
 class ItemSerializer(serializers.ModelSerializer):
-               
+    category = CategorySerializer()
+    # category = serializers.SerializerMethodField()
+    # def get_category(self, obj):
+    #     categories = obj.category.name
+    #     return categories
+
+    def create(self, validated_data):
+        # validated_data = {
+        #   'name' : 'cheeze pizza',
+        #   'category' : {
+        #       'name' : 'pizza'
+        #   }
+        # }
+        category_data = validated_data.pop('category') # {'name':'pizza'}
+        category_name = category_data.get('name')   # pizza
+        category_obj = CategoryModel.objects.get(name=category_name)    # object
+        
+        item = ItemModel(category=category_obj, **validated_data)
+        item.save()
+        return item
+
     class Meta:
         model = ItemModel
-        fields = ['name','category','image_url']
+        fields = ["name","category","image_url","category_id"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
-               
+    item = ItemSerializer(many=True)
     class Meta:
         model = OrderModel
-        fields = ['delivery_address', 'order_date','item']
+        fields = ["delivery_address","order_date","item"]
 
 
-class ItemOderSerializer(serializers.ModelSerializer):
-               
+class ItemOrderSerializer(serializers.ModelSerializer):
+                
     class Meta:
         model = ItemOrderModel
-        fields = ['order','item','item_count']
+        fields = ["order","item","item_count"]
--- item/views.py
+++ item/views.py
@@ -2,11 +2,54 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Category
-from .models import Item
-from .models import Order
-from .models import ItemOrder
+from .models import Category as CategoryModel
+from .models import Item as ItemModel
+from .models import Order as OrderModel
+from .models import ItemOrder as ItemOrderModel
+
+from .serializers import CategorySerializer
+from .serializers import ItemSerializer
+from .serializers import OrderSerializer
+from .serializers import ItemOrderSerializer
+
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.db.models.query_utils import Q
 # Create your views here.
 
+class ItemView(APIView):
 
+    def get(self, request):
+        # category = request.GET.get('category', None)
+        category = request.GET['category']  # appliance
 
+        # items = ItemModel.objects.filter(category__name=category)
+        category = CategoryModel.objects.get(name=category) # object(2)
+        items = ItemModel.objects.filter(category_id=category)
+        
+        if items.exists():
+            item_serializer = ItemSerializer(items, many=True).data
+            return Response(item_serializer)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
+    def post(self, request):
+        # item = request.data
+        # print(request.data)
+        item_serializer = ItemSerializer(data=request.data)
+        if item_serializer.is_valid():
+            item_serializer.save()
+            return Response(item_serializer.data, status=status.HTTP_200_OK)
+        return Response(item_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class OrderView(APIView):
+
+    def get(self, request):
+        order_id = request.GET['order_id']
+        
+        time = timezone.now() - timedelta(days=1)
+        query = Q(id=order_id)|Q(order_date__gte=time)
+
+        order_objs = OrderModel.objects.filter(query)
+        return Response(OrderSerializer(order_objs, many=True).data)
