f1c0b733f180eb4922c875400ebbff4df91c1722
mankic <miic0803@gmail.com>
2022-06-23 09:50:35 +0900

    수정 - serializer base

--- item/urls.py
+++ item/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from item import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('item/', include('item.urls')),
+    path('', views.ItemView.as_view()),
 ]
